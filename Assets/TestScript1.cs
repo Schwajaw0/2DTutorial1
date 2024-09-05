6aecd63ee8d94c9b9e218d7480ac1b33cc6fd7ed
Schwajaw0 <Jackschwartz07@gmail.com>
2024-09-05 18:54:23 -0400

    2DTutorial

--- Assets/TestScript1.cs
+++ Assets/TestScript1.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class TestScript1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
